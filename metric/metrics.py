9c8b0e2c5ad26ae2ffb40831954b6a63aa0fa3f5
erencantatar <erencantatar@gmail.com>
2023-10-08 19:05:51 +0200

    Update metrics.py

--- metric/metrics.py
+++ metric/metrics.py
@@ -1,113 +1,178 @@
-"Eval Metric"
-
-# import evaluate
-# rouge = evaluate.load("rouge")
-# metric = load("rouge")
-import torch
 import numpy as np
 import nltk
+import torch
 
-from .summac.summac.model_summac import SummaCZS, SummaCConv
+from summac.model_summac import SummaCZS, SummaCConv
 from factsumm import FactSumm
+import os
+from transformers import pipeline
+from bleurt.score import BleurtScorer
+import wandb
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
 
-def compute_metrics(tokenizer, document_texts_batch, summary_texts_batch, 
-                    summary_texts_pred_batch, device, NumberOfEvals, print_QA = False):
+# TODO: Change to the BLEURT-20 model instead!
+BLEURT_MODEL = "metric/bleurt/BLEURT-20-D3"
+if os.path.exists(BLEURT_MODEL):
+    checkpoint = BLEURT_MODEL
+elif os.path.exists("metric/bleurt/bleurt/BLEURT-20/"):
+    checkpoint = "metric/bleurt/bleurt/BLEURT-20/" 
+else:
+    print("BLEURT MODEL NOT FOUND")
+    checkpoint = None
 
+factsumm = FactSumm()
+bleurt_scorer = BleurtScorer(checkpoint=checkpoint)
+summac_scorer = SummaCConv(models=["vitc"], bins='percentile', granularity="sentence",
+               nli_labels="e", device=device, start_file="default", agg="mean")
+
+
+def compute_metrics_pipeline(articles, summaries):
+    qags_scores = []
+    rouge_scores = []
+    fact_scores = []
+    bleurt_scores = []
+    summac_scores = []
+    ensemble_scores = []
+
+    # try:
+    #     bleurt_scores = bleurt_scorer.score(references=articles, candidates=summaries)
+    # except Exception as ex:
+    #     # TODO: bad idea if only one fails out of all of them?
+    #     print(f"BLEURT ERROR! {ex}")
+    #     bleurt_scores = [0] * len(articles)
+    #
+    # try:
+    #     summac_scores = summac_scorer.score(articles, summaries)
+    # except Exception as ex:
+    #     print(f"SUMMAC ERROR! {ex}")
+    #     summac_scores = [0] * len(articles)
+
+    for i in range(len(articles)):
+        article, summary = articles[i], summaries[i]
+        try:
+            qags = factsumm.extract_qas(article, summary, device=device)
+        except TypeError:
+            # TODO: BIAS! Take care of this?
+            qags = 0
+        qags_scores.append(qags)
+
+        try:
+            rouge = factsumm.calculate_rouge(article, summary)[-1] # Take ROUGE-L
+        except Exception as ex:
+            print(f"ROUGE ERROR! {ex}")
+            rouge = 0
+        rouge_scores.append(rouge)
+
+        # Doesn't work, idk
+        # BERT = factsumm.calculate_bert_score(article, summary)
+
+        try:
+            facts = factsumm.extract_facts(article, summary, device=device)[-1]
+        except Exception as ex:
+            print(f"FACTS ERROR! {ex}")
+            facts = 0
+        fact_scores.append(facts)
+
+        references = [article]
+        candidates = [summary]
+        try:
+            bleurt_score = bleurt_scorer.score(references=references,
+                                               candidates=candidates)[0]
+        except Exception as ex:
+            print(f"BLEURT ERROR! {ex}")
+            bleurt_score = 0
+        bleurt_scores.append(bleurt_score)
+
+        try:
+            summac_score = summac_scorer.score(references, candidates)['scores'][0]
+        except Exception as ex:
+            print(f"SUMMAC ERROR! {ex}")
+            summac_score = 0
+        summac_scores.append(summac_score)
+
+        ensemble = np.mean([qags, rouge, facts, bleurt_score, summac_score])
+        ensemble_scores.append(ensemble)
+
+    # Calculate the average ensemble score over the batch
+    # batch_ensemble_score = np.mean(ensemble_scores)
+
+    return {
+        "qags": qags_scores,
+        "rouge": rouge_scores,
+        "triples": fact_scores,
+        # "BERT": BERT,
+        "bleurt": bleurt_scores,
+        "summac": summac_scores,
+        "ensemble": ensemble_scores,
+    }
+
+
+def compute_metrics(
+    tokenizer,
+    document_texts_batch,
+    summary_texts_batch,
+    summary_texts_pred_batch,
+    device,
+):
     num_samples = len(document_texts_batch)
     ensemble_scores = []
 
-    torch.cuda.empty_cache()
     factsumm = FactSumm()
 
-
-    print("-------- Start evaluation --------")
-    print("num_samples", num_samples)
-
-    batch_scores = {
-        "QA_based": [],
-        "ROUGE": [],
-        "BERT": [],
-        "BLEURT": [],
-        "SUMMAC": [],
-        "ensemble": []
-    }
-    
-    # overwrites the number of evals if the batch size is smaller
-    if NumberOfEvals < num_samples:
-        num_samples = NumberOfEvals  
-    
-    for i in range(0, num_samples):
+    for i in range(num_samples):
         article = document_texts_batch[i]
         summary_texts = summary_texts_batch[i]  # List of vocab IDs
         summary_pred_ids = summary_texts_pred_batch[i]  # List of vocab IDs
-        
+
         # PREDICTED
         # Decode the summary_ids and summary_pred_ids to text summaries
-        # print("summary_pred_ids", summary_pred_ids)
         summary_pred_text = tokenizer.decode(summary_pred_ids, skip_special_tokens=True)
-        # print("summary_pred_text", summary_pred_text)
-        summary_pred_text = summary_pred_text if type(summary_pred_text) == list else summary_pred_text
-        
+
         article, summary_texts = str(article), str(summary_texts)
-        # print()
+        print(f"article {type(article)}\n", article)
+        print(f"summary_texts {type(summary_texts)}\n", summary_texts)
+        print("summary", summary_pred_text)
         summary = summary_pred_text
 
-        try:
-            QA_BASED = factsumm.extract_qas(article, summary, verbose=print_QA)
-        except:
-            QA_BASED = None
-        print("QA_BASED", QA_BASED)
-        torch.cuda.empty_cache()
+        print_QA = False
 
-        R1, R2, R_L = factsumm.calculate_rouge(article, summary)
-        ROUGE = R1
-        print("ROUGE", ROUGE)
-        torch.cuda.empty_cache()
+        print("device", device)
 
-        BERT, BLEURT = 0, 0
-        # BERT = factsumm.calculate_bert_score(article, summary)
-        print("BERT", BERT)
-        # torch.cuda.empty_cache()
-        # F1, Precsion, Recall
-        # from bleurt import score
-        # import os
-        # # check in model for BLEURT-20-D3 or BLEURT-20    
-        # if os.path.exists("metric/bleurt/bleurt/BLEURT-20-D3/"):
-        #     checkpoint = "metric/bleurt/bleurt/BLEURT-20-D3/"
-        # else:
-        #     checkpoint = "metric/bleurt/bleurt/BLEURT-20/"
-        # scorer = score.BleurtScorer(checkpoint)
-        # BLEURT = scorer.score(references=[article], candidates=[summary])
-        # print("BLEURT", BLEURT)
-        # torch.cuda.empty_cache()
+        QA_based = factsumm.extract_qas(article, summary)
+        ROUGE = factsumm.calculate_rouge(article, summary)
+        facts = factsumm.extract_facts(article, summary)
+        # BERT = factsumm.calculate_bert_score(article, summary) #  device="cuda")
+        BERT = 1
 
-        torch.cuda.empty_cache()
+        # Load the BLEURT scorer model
+        checkpoint = "../bleurt/BLEURT-20"
+        bleurt_scorer = pipeline(
+            task="table-question-generation", model=checkpoint, device=device
+        )
+        # Define your reference and candidate sentences
+        references = [summary_texts]
+        candidates = [summary_pred_text]
+        # Compute BLEURT scores for the candidates
+        BLEURT = bleurt_scorer(references=references, candidates=candidates)
+
         model_conv = SummaCConv(models=["vitc"], bins='percentile', granularity="sentence", nli_labels="e", device=device, start_file="default", agg="mean")
-        SUMMAC = model_conv.score([article], [summary])['scores'][0]
-        print("SUMMAC", SUMMAC)
-        torch.cuda.empty_cache()
+        SUMMAC = model_conv.score([article], [summary], device=device)
 
-        # update scores
-        batch_scores["QA_based"].append(QA_BASED)
-        batch_scores["ROUGE"].append(ROUGE)
-        batch_scores["BERT"].append(BERT)
-        batch_scores["BLEURT"].append(BLEURT)
-        batch_scores["SUMMAC"].append(SUMMAC)
-        print(QA_BASED, ROUGE, BERT, BLEURT, SUMMAC)
-        combine = [QA_BASED, ROUGE, BERT, BLEURT, SUMMAC]
-        filtered_list = list(filter(lambda x: x is not None, combine))
-        batch_scores["ensemble"].append(np.mean(filtered_list))
-        
-        # print("batch_scores", batch_scores)
+        ensemble = np.mean([QA_based, ROUGE, BERT, BLEURT, SUMMAC])
+
+        ensemble_scores.append(ensemble)
+
     # Calculate the average ensemble score over the batch
-
-    filtered_list_QA = list(filter(lambda x: x is not None, batch_scores["QA_based"]))
+    batch_ensemble_score = np.mean(ensemble_scores)
 
     return {
-        "QA_based": np.mean(filtered_list_QA),
-        "ROUGE": np.mean(batch_scores["ROUGE"]),
-        "BERT": np.mean(batch_scores["BERT"]),
-        "BLEURT": np.mean(batch_scores["BLEURT"]),
-        "SUMMAC": np.mean(batch_scores["SUMMAC"]),
-        "ensemble": np.mean(batch_scores["ensemble"]),  # Return the average ensemble score
+        "QA_based": QA_based,
+        "ROUGE": ROUGE,
+        "Triples": facts,
+        "BERT": BERT,
+        "BLEURT": BLEURT,
+        "SUMMAC": SUMMAC,
+        "ensemble": batch_ensemble_score,  # Return the average ensemble score
     }
